f5100fe915338d88dcd84782f13e7e14867e6f9c
Evan Overman <evanrileyoverman@gmail.com>
2020-08-05 14:37:52 -0700

    Edits to server.js to make easier to read

--- serverStart.js
+++ serverStart.js
@@ -25,12 +25,5 @@ app.get('/stylesheet.css', (req, res) => {
 
 });
 
-// This would be at http://your.ip:yourport/dir.
-// You can repeat this as many times as needed.
-
-app.get('/dir', (req, res) => {
-    res.send('Another page');
-});
-
 // Set 3000 to whatever port you would like it to be hosted on.
 app.listen(3000, console.log('Server listening on port 3000'));
